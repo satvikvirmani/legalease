361952ba7736d496efaee617ffd9f0777885e015
Satvik Virmani <virmanisatvik01@gmail.com>
2024-04-03 12:28:18 +0530

    updated seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,24 +1,4 @@
 const { db } = require('@vercel/postgres');
-const {
-  requests,
-  providers,
-  consumers,
-} = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
-
-async function hashPassword(user) {
-  const password = user.password;
-  const saltRounds = 10;
-
-  const hashedPassword = await new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-      if (err) reject(err);
-      resolve(hash);
-    });
-  });
-
-  return hashedPassword;
-}
 
 async function seedConsumers(client) {
   try {
@@ -40,7 +20,7 @@ async function seedConsumers(client) {
       createTable,
     };
   } catch (error) {
-    console.error('Error seeding consumers:', error);
+    console.error('Error creating consumers:', error);
     throw error;
   }
 }
@@ -67,25 +47,11 @@ async function seedProviders(client) {
 
     console.log(`Created "providers" table`);
 
-    // Insert data into the "users" table
-    // const insertedProviders = await Promise.all(
-    //   providers.map(async (provider) => {
-    //     const hashedPassword = await hashPassword(provider);
-    //     return client.sql`
-    //     INSERT INTO providers (id, name, email, password, title, description, speciality)
-    //     VALUES (${provider.id}, ${provider.name}, ${provider.email}, ${hashedPassword}, ${provider.title}, ${provider.description}, ${provider.speciality})
-    //     ON CONFLICT (id) DO NOTHING;
-    //   `;
-    //   }),
-    // );
-
-    //console.log(`Seeded ${insertedProviders.length} providers`);
-
     return {
       createTable,
     };
   } catch (error) {
-    console.error('Error seeding providers:', error);
+    console.error('Error creating providers:', error);
     throw error;
   }
 }
@@ -93,7 +59,6 @@ async function seedProviders(client) {
 async function seedRequests(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
     const createTable = await client.sql`
       CREATE TABLE IF NOT EXISTS requests (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -108,24 +73,11 @@ async function seedRequests(client) {
 
     console.log(`Created "requests" table`);
 
-    // Insert data into the "users" table
-    // const insertedRequests = await Promise.all(
-    //   requests.map(async (request) => {
-    //     return client.sql`
-    //     INSERT INTO requests (id, consumerId, providerId, description)
-    //     VALUES (${request.id}, ${request.consumerId}, ${request.providerId}, ${request.description})
-    //     ON CONFLICT (id) DO NOTHING;
-    //   `;
-    //   }),
-    // );
-
-    //console.log(`Seeded ${insertedRequests.length} requests`);
-
     return {
       createTable,
     };
   } catch (error) {
-    console.error('Error seeding requests:', error);
+    console.error('Error creating requests:', error);
     throw error;
   }
 }
@@ -142,7 +94,7 @@ async function main() {
 
 main().catch((err) => {
   console.error(
-    'An error occurred while attempting to seed the database:',
+    'An error occurred while attempting to create the database:',
     err,
   );
 });
