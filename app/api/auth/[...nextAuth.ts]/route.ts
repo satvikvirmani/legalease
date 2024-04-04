719ffcf2c1d88980ab9a94e7bb2b20b3904ec95c
Satvik Virmani <virmanisatvik01@gmail.com>
2024-04-04 10:12:43 +0530

    qwe

--- app/api/auth/[...nextAuth.ts]/route.ts
+++ app/api/auth/[...nextAuth.ts]/route.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
+// import NextAuth from "next-auth";
 
-export const authOptions = {
-  providers: [
-  ]};
+// export const authOptions = {
+//   providers: [
+//   ]};
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
